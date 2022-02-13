105a986c328b2caad0350c81f32836ae24f151c8
Hannes Westberg <westberg.hannes@gmail.com>
2022-02-13 14:35:02 +0100

    Updated SpotifyRepository with smarter access token renewal

--- src/SpotifyRecommendations.Infrastructure/Spotify/Repositories/SpotifyRepository.cs
+++ src/SpotifyRecommendations.Infrastructure/Spotify/Repositories/SpotifyRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Headers;
+﻿using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Net.Mime;
 using System.Text;
@@ -14,46 +15,60 @@ public class SpotifyRepository : ISpotifyRepository
 {
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly IOptions<SpotifyOptions> _options;
-    
+
+    private static readonly TimeSpan AccessTokenLifeTime = new(0, 45, 0);
+    private DateTime _accessTokenTimeStamp = DateTime.MinValue;
+
     public SpotifyRepository(IHttpClientFactory httpClientFactory, IOptions<SpotifyOptions> options)
     {
         _httpClientFactory = httpClientFactory;
         _options = options;
-    }
-    
-    private async Task<AuthorizationResponseDto> Auth(CancellationToken cancellationToken = default)
-    {
-        var httpClient = _httpClientFactory.GetClientByUri(new Uri(_options.Value.AuthBaseAddress!));
-
+        
+        var authHttpClient = _httpClientFactory.GetClientByUri(new Uri(_options.Value.AuthBaseAddress!));
         var authKeyText = $"{_options.Value.ClientId}:{_options.Value.ClientSecret}";
         var authKeyBytes = Encoding.ASCII.GetBytes(authKeyText);
         var authKeyEncoded = Convert.ToBase64String(authKeyBytes);
-        
-        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authKeyEncoded);
+        authHttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authKeyEncoded);
 
-        var response = await httpClient.PostAsync(_options.Value.Endpoints!.RequestAuthorization,
+        var requestHttpClient = _httpClientFactory.GetClientByUri(new Uri(_options.Value.BaseAddress!));
+        requestHttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaTypeNames.Application.Json));
+    }
+    
+    public async Task<T?> Get<T>(string url, CancellationToken cancellationToken = default)
+    {
+        if (!AccessTokenIsValid())
+            await Auth(cancellationToken);
+        
+        var httpClient = _httpClientFactory.GetClientByUri(new Uri(_options.Value.BaseAddress!));
+        
+        var response = await httpClient.GetAsync(url, cancellationToken);
+
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadFromJsonAsync<T>(cancellationToken: cancellationToken);
+    }
+    
+    private async Task Auth(CancellationToken cancellationToken = default)
+    {
+        var authHttpClient = _httpClientFactory.GetClientByUri(new Uri(_options.Value.AuthBaseAddress!));
+        
+        var response = await authHttpClient.PostAsync(_options.Value.Endpoints!.RequestAuthorization,
             new FormUrlEncodedContent(new [] {
                 new KeyValuePair<string, string>("grant_type", "client_credentials")
             }), cancellationToken);
 
         response.EnsureSuccessStatusCode();
         
-        return (await response.Content.ReadFromJsonAsync<AuthorizationResponseDto>(cancellationToken: cancellationToken))!;
+        var accessToken = await response.Content.ReadFromJsonAsync<AuthorizationResponseDto>(cancellationToken: cancellationToken);
+        _accessTokenTimeStamp = DateTime.Now;
+
+        var requestHttpClient = _httpClientFactory.GetClientByUri(new Uri(_options.Value.BaseAddress!));
+        
+        requestHttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken!.AccessToken);
     }
-    
-    public async Task<T?> Get<T>(string url, CancellationToken cancellationToken = default)
+
+    private bool AccessTokenIsValid()
     {
-        // Todo: Make a smarter authentication. No need to authenticate on each request
-        var bearer = await Auth(cancellationToken);
-
-        var httpClient = _httpClientFactory.GetClientByUri(new Uri(_options.Value.BaseAddress!));
-        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearer.AccessToken);
-        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaTypeNames.Application.Json));
-
-        var response = await httpClient.GetAsync(url, cancellationToken);
-
-        response.EnsureSuccessStatusCode();
-
-        return await response.Content.ReadFromJsonAsync<T>(cancellationToken: cancellationToken);
+        return DateTime.Now - _accessTokenTimeStamp < AccessTokenLifeTime;
     }
 }
